4674e6a314ed4ac913782b796a8f2f974d085e6e
Rodrigo Casale <popobasura3@hotmail.com>
2023-08-08 22:23:42 -0600

    initial files

---
+++ include/scene_type.h
@@ -0,0 +1,15 @@
+
+#ifndef BF_SCENE_TYPE_H
+#define BF_SCENE_TYPE_H
+
+namespace game
+{
+    enum class scene_type
+    {
+        TITLE,
+        MENU,
+        GAME
+    };
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include "bn_core.h"
 
+#include "scene_type.h"
+
+#include "title_screen.h"
+
 int main()
 {
     bn::core::init();
 
-    while(true)
+    game::scene_type scene = game::scene_type::TITLE;
+
+    while (true)
     {
+        switch (scene)
+        {
+        case game::scene_type::TITLE:
+            game::title_screen();
+            break;
+
+        default:
+            break;
+        }
         bn::core::update();
     }
 }
